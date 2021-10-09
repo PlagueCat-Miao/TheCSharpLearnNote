1eaeedda76520338be6ad9a6664801dfced806bd
PlagueCat-Miao <ace0413@126.com>
2021-10-09 19:09:43 +0800

    test

---
+++ TheCSharpLearnNote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheCSharpLearnNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
